f54780fdb8775a5e3c5d9a605a9358550bb090db
RealitySlice <armandsumo@gmail.com>
2022-10-18 05:48:48 +0200

    add hyperstep gh page boilerplate

---
+++ src/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+ module.exports = {
+   entry: './src/index.js',
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   module: {
+     rules: [
+       {
+         test: /\.css$/i,
+         use: ['style-loader', 'css-loader'],
+       },
+       {
+         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+         type: 'asset/resource',
+       },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+         type: 'asset/resource',
+       },
+
+      {
+
+        test: /\.(csv|tsv)$/i,
+
+        use: ['csv-loader'],
+
+      },
+
+      {
+
+        test: /\.xml$/i,
+
+        use: ['xml-loader'],
+
+      },
+     ],
+   },
+ };
+
