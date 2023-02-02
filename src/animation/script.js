52994c948f58d53f464f8873d21dc78044c9d0a6
a-sumo <armandsumo@gmail.com>
2023-02-02 03:46:16 +0100

    test animation timeline

--- src/animation/script.js
+++ src/animation/script.js
@@ -3,6 +3,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
+import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry.js';
 import anime from 'animejs/lib/anime.es.js';
 
 let camera, 
@@ -20,6 +22,8 @@ object3
 
 let timeline
 
+let instructions, behavior, behavioral_attention
+
 init();
 animate();
 
@@ -28,9 +32,9 @@ function init() {
     scene = new THREE.Scene()
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.x = 4
-    camera.position.y = 4
-    camera.position.z = 4
+    camera.position.x = 7
+    camera.position.y = 7
+    camera.position.z = 7
 
     renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight)
@@ -39,73 +43,106 @@ function init() {
     controls = new OrbitControls(camera, renderer.domElement)
     controls.target.set(8, 0, 0)
 
-    const light1 = new THREE.PointLight()
-    light1.position.set(10, 10, 10)
-    scene.add(light1)
-
-    const light2 = new THREE.PointLight()
-    light2.position.set(-10, 10, 10)
-    scene.add(light2)
-
     object1 = new THREE.Mesh(
-        new THREE.BoxGeometry(),
-        new THREE.MeshPhongMaterial({ color: 0xff0000 })
+        new RoundedBoxGeometry( 2, 2, 2, 7, 0.4 ),
+        new THREE.MeshStandardMaterial( { roughness: 0.1} )
     )
-    object1.position.set(4, 0, 0)
+    
+    object1.position.set(0, 0, 0)
     scene.add(object1)
     console.log(object1.position, object1.rotation)
 
     object1.add(new THREE.AxesHelper(5))
 
     object2 = new THREE.Mesh(
-        new THREE.BoxGeometry(),
-        new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+        new RoundedBoxGeometry( 2, 2, 2, 7, 0.4 ),
+        new THREE.MeshStandardMaterial( { roughness: 0.1} )
     )
     object2.position.set(4, 0, 0)
     object1.add(object2)
     object2.add(new THREE.AxesHelper(5))
 
     object3 = new THREE.Mesh(
-        new THREE.BoxGeometry(),
-        new THREE.MeshPhongMaterial({ color: 0x0000ff })
+        new RoundedBoxGeometry( 2, 2, 2, 7, 0.4 ),
+        new THREE.MeshStandardMaterial( { roughness: 0.1 } )
     )
     object3.position.set(4, 0, 0)
     object2.add(object3)
     object3.add(new THREE.AxesHelper(5))
+
+    // define instructions as a string of characters from 'a to c'
+    
+    //vocabulary = ["a1", "a2", "b1", "b2", "c1", "c2"]
+    instructions = ["a1", "a1"]
+    // the reponse to inputs defines the behavior
+    behavior = {
+        "a1" : {
+            targets: object1.rotation,
+            x: object1.rotation.x - Math.PI/2,
+            y: object1.rotation.y - 0,
+            z: object1.rotation.z - 0,
+            duration: 200,
+            easing: 'spring',
+            update: camera.updateProjectionMatrix()
+        }, 
+        "a2" : {
+            targets: object1.rotation,
+            x: object1.rotation.x - 0,
+            y: object1.rotation.y - 0,
+            z: object1.rotation.z - 0,
+            duration: 500,
+            easing: 'linear',
+            update: camera.updateProjectionMatrix()
+        }, 
+        "b1": {
+            targets: object2.rotation,
+            x: 0,
+            y: -Math.PI/2,
+            z: 0,
+            duration: 400,
+            easing: 'linear',
+            update: camera.updateProjectionMatrix()
+    },
+        "b2": {
+            targets: object2.rotation,
+            x: 0,
+            y: 0,
+            z: Math.PI/2,
+            duration: 1000,
+            easing: 'linear',
+            update: camera.updateProjectionMatrix()
+    },
+        "c1": {
+            targets: object1.rotation,
+            x: 0,
+            y: 0,
+            z: -Math.PI/2,
+            duration: 100,
+            easing: 'linear',
+            update: camera.updateProjectionMatrix()
+        },
+        "c2": {
+            targets: object1.rotation,
+            x: Math.PI/2,
+            y: 0,
+            z: 0,
+            duration: 1000,
+            easing: 'linear',
+            update: camera.updateProjectionMatrix()
+        }
+    }
+    // the behavioral attention defines the parts being actuated 
+    behavioral_attention = [[0,1], [1], [2], []]
+
+    // create timeline
     timeline = anime.timeline({
         autoplay: true,
-        duration: 4500,
+        duration: instructions-length * 500,
         easing: 'easeOutSine'
     });
-    timeline.add({
-        targets: object1.rotation,
-        x: -Math.PI/2,
-        y: 0,
-        z: 0,
-        loop: 3,
-        duration: 500,
-        easing: 'linear',
-        update: camera.updateProjectionMatrix()
-    })
-    timeline.add({
-        targets: object1.rotation,
-        x: Math.PI/2,
-        y: 0,
-        z: 0,
-        duration: 500,
-        easing: 'linear',
-        update: camera.updateProjectionMatrix()
-    })
-    timeline.add({
-        targets: object2.rotation,
-        x: 0,
-        y: Math.PI/2,
-        z: 0,
-        duration: 500,
-        easing: 'linear',
-        update: camera.updateProjectionMatrix()
-    });
-
+    for (let i = 0; i < instructions.length; i++) {
+        timeline.add(behavior[instructions[i]])
+    }
 
     window.addEventListener('resize', onWindowResize, false)
 
@@ -136,8 +173,11 @@ function init() {
     stats = Stats()
     document.body.appendChild(stats.dom)
 
-    // geometry
+    const environment = new RoomEnvironment();
+    const pmremGenerator = new THREE.PMREMGenerator( renderer );
 
+    scene.environment = pmremGenerator.fromScene( environment ).texture;
+    scene.background =  new THREE.Color("rgb(100, 100,100)") ;
     stats = new Stats();
     document.body.appendChild( stats.dom );
 
